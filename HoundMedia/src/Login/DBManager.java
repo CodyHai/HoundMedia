9ce2cbe4c8029c0ae936b98280672f8ffa8b8c1e
Avishikta Bagchi <118616545+AvishiktaBagchi@users.noreply.github.com>
2025-04-03 19:20:33 -0500

    Adding Login_code

---
+++ HoundMedia/src/Login/DBManager.java
@@ -0,0 +1,34 @@
+package Login;
+import java.util.ArrayList;
+
+public class DBManager 
+{
+    ArrayList<User> usersList; // contains list of User objects
+   
+    public DBManager()
+    {
+        usersList = new ArrayList<>();
+    }
+
+    public ArrayList<User> arrayList()
+    {
+        return usersList;
+    }
+
+    public User getUserAti(int i)
+    {
+        return usersList.get(i);
+    }
+
+    //populates database for testing
+    public void populateDB()
+    {
+        User user1 = new User("bobThebuilder", "i_love_building!123");
+        User user2 = new User("hello_kitty123", "cat_lover456!");
+        User user3 = new User("avi", "password");
+
+        usersList.add(user1);
+        usersList.add(user2);
+        usersList.add(user3);
+    }
+}
